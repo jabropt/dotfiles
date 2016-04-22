70401a6f6aff22af0bad1f0d9a80670c9e2fcf50
itodaisuke <jabropt@gmail.com>
2016-04-22 18:14:45 +0900

    helm-gtags setting

--- .emacs.d/inits/30_helm.el
+++ .emacs.d/inits/30_helm.el
@@ -12,3 +12,16 @@
 (global-set-key (kbd "C-x C-f") 'helm-find-files)
 (global-set-key (kbd "C-x C-r") 'helm-recentf)
 (global-set-key (kbd "C-c C-p") 'helm-projectile)
+
+;; automatically start gtags-mode on ruby-mode
+(add-hook 'ruby-mode-hook (lambda () (helm-gtags-mode)))
+
+;; key bindings
+(eval-after-load "helm-gtags"
+  '(progn
+     (define-key helm-gtags-mode-map (kbd "M-t") 'helm-gtags-find-tag)
+     (define-key helm-gtags-mode-map (kbd "M-r") 'helm-gtags-find-rtag)
+     (define-key helm-gtags-mode-map (kbd "M-s") 'helm-gtags-find-symbol)
+     (define-key helm-gtags-mode-map (kbd "C-c <") 'helm-gtags-previous-history)
+     (define-key helm-gtags-mode-map (kbd "C-c >") 'helm-gtags-next-history)
+     (define-key helm-gtags-mode-map (kbd "M-,") 'helm-gtags-pop-stack)))
