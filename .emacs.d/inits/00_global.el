58bb3648354cec454148a090ca155e48869df3bd
itodaisuke <jabropt@gmail.com>
2016-04-24 15:08:06 +0900

    Add copy and paste, emacs <-> osx

--- .emacs.d/inits/00_global.el
+++ .emacs.d/inits/00_global.el
@@ -20,6 +20,19 @@
 (global-set-key (kbd "C-z") 'undo)
 (define-key input-decode-map (kbd "C-h") (kbd "DEL"))
 
+;; kill and yank
+(defun copy-from-osx ()
+  (shell-command-to-string "pbpaste"))
+
+(defun paste-to-osx (text &optional push)
+  (let ((process-connection-type nil))
+    (let ((proc (start-process "pbcopy" "*Messages*" "pbcopy")))
+      (process-send-string proc text)
+      (process-send-eof proc))))
+
+(setq interprogram-cut-function 'paste-to-osx)
+(setq interprogram-paste-function 'copy-from-osx)
+
 ;; hooks
 (add-hook 'before-save-hook 'delete-trailing-whitespace)
 
